319e68dceffc47317cd4eb8cffd5befde8c6b424
Kaustubh <kaustubh8856@gmail.com>
2025-06-07 22:30:44 +0530

    resturant model created

---
+++ models/resturantModel.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+// schema
+const resturantSchema = new mongosse.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Resturant title is required."],
+    },
+    imageUrl: {
+      type: String,
+    },
+    foods: { type: Array },
+    time: {
+      type: String,
+    },
+    pickup: {
+      type: Boolean,
+      default: true,
+    },
+    delivery: {
+      type: Boolean,
+      default: true,
+    },
+    isOpen: {
+      type: Boolean,
+      default: true,
+    },
+    logoUrl: {
+      type: String,
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      default: 1,
+    },
+    ratingCount: { type: String },
+    code: { type: String },
+    coords: {
+      id: { type: String },
+      address: { type: String },
+      title: { type: String },
+      latitude: { type: Number },
+      latitudeDelta: { type: Number },
+      longitude: { type: Number },
+      longitudeDelta: { type: Number },
+    },
+  },
+  { timestamps: true }
+);
+
+//export
+module.exports = mongoose.model("Resturant", resturantSchema);
