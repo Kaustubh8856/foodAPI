7132d747b20a4f4e0ec77bf8aa739a517822ab70
Kaustubh <kaustubh8856@gmail.com>
2025-06-08 13:52:57 +0530

    implement get all and single resturant

--- controllers/resturantControllers.js
+++ controllers/resturantControllers.js
@@ -54,5 +54,66 @@ const createResturantController = async (req, res) => {
   }
 };
 
+// GET ALL RESTURANT CONTROLLER
+const getAllResturantController = async (req, res) => {
+  try {
+    // find all resturants
+    const resturants = await resturantModel.find({});
+    // validation
+    if (!resturants) {
+      return res.status(404).send({
+        success: False,
+        message: "No resturants found.",
+      });
+    }
+    res.status(200).send({
+      success: true,
+      totalCount: resturants.length,
+      resturants,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "Error in get all resturant API",
+      error,
+    });
+  }
+};
+
+// GET RESTURANT BY ID CONTROLLER
+const getResturantByIdController = async (req, res) => {
+  try {
+    const resturantId = req.params.id;
+    if (!resturantId) {
+      return res.status(404).send({
+        success: false,
+        message: "Please provide resturant ID",
+      });
+    }
+    const resturant = await resturantModel.findById(resturantId);
+    if (!resturant) {
+      return res.status(404).send({
+        success: false,
+        message: "Resturant not found",
+      });
+    }
+    res.status(200).send({
+      success: true,
+      message: "Resturant found successfully",
+      resturant,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "Error in get resturant by ID API.",
+    });
+  }
+};
 // exports
-module.exports = { createResturantController };
+module.exports = {
+  createResturantController,
+  getAllResturantController,
+  getResturantByIdController,
+};
--- routes/resturantRoutes.js
+++ routes/resturantRoutes.js
@@ -2,12 +2,20 @@ const express = require("express");
 const authMiddleware = require("../middlewares/authMiddleware");
 const {
   createResturantController,
+  getAllResturantController,
+  getResturantByIdController,
 } = require("../controllers/resturantControllers");
 const router = express.Router();
 
 // routes
 // CREATE RESTURANT || POST
 router.post("/create", authMiddleware, createResturantController);
+
+// GET ALL RESTURANT || GET
+router.get("/getAll", getAllResturantController);
+
+// GET RESTURANT BY ID ROUTE || GET
+router.get("/getResturantID/:id", getResturantByIdController);
 
 // exports
 module.exports = router;
