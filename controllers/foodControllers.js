68a4ca3bb08b178c90ce2adee4fe1ecc9b1b5e80
Kaustubh <kaustubh8856@gmail.com>
2025-06-09 17:25:51 +0530

    get food by id

--- controllers/foodControllers.js
+++ controllers/foodControllers.js
@@ -1,5 +1,6 @@
 const foodModal = require("../models/foodModal");
 
+// CREATE FOOD CONTROLLER
 const createFoodController = async (req, res) => {
   try {
     const {
@@ -49,5 +50,66 @@ const createFoodController = async (req, res) => {
   }
 };
 
+// GET ALL FOODS CONTROLLER
+const getAllFoodController = async (req, res) => {
+  try {
+    const foods = await foodModal.find({});
+    if (!foods) {
+      return res.status(404).send({
+        success: false,
+        message: "No foods found!",
+      });
+    }
+    res.status(200).send({
+      success: true,
+      message: "Foods found.",
+      TotalFoodItems: foods.length,
+      foods,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "Error in get all food API!",
+      error,
+    });
+  }
+};
+
+// GET FOOD BY ID CONTROLLER
+const getFoodByIdCONTROLLER = async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      return res.status(500).send({
+        success: false,
+        message: "Please provide the ID!",
+      });
+    }
+    const food = await foodModal.findById(id);
+    if (!food) {
+      return res.status(404).send({
+        success: false,
+        message: "Food item not found.",
+      });
+    }
+    res.status(200).send({
+      success: true,
+      message: "Food Item found",
+      food,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      messgae: "Error in get food by ID API!",
+      error,
+    });
+  }
+};
 // exports
-module.exports = { createFoodController };
+module.exports = {
+  createFoodController,
+  getAllFoodController,
+  getFoodByIdCONTROLLER,
+};
--- routes/foodRoutes.js
+++ routes/foodRoutes.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const authMiddleware = require("../middlewares/authMiddleware");
-const { createFoodController } = require("../controllers/foodControllers");
+const {
+  createFoodController,
+  getAllFoodController,
+  getFoodByIdCONTROLLER,
+} = require("../controllers/foodControllers");
 
 const router = express.Router();
 
 //ROUTES
 // CREATE FOOD ROUTE
 router.post("/create", authMiddleware, createFoodController);
+
+// GET ALL FOODS ROUTE
+router.get("/getAll", getAllFoodController);
+
+// GET FOOD BY ID ROUTE
+router.get("/get/:id", getFoodByIdCONTROLLER);
 
 // EXPORTS
 module.exports = router;
